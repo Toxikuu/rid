30cb89bd58c027856f946d7f6fdc3ef4e9928ef7
Tox <toxikuu@users.noreply.github.com>
2024-10-06 02:18:45 -0500

    simplified

--- src/paths.rs
+++ src/paths.rs
@@ -1,14 +1,9 @@
 // src/paths.rs
 
-use project_root::get_project_root;
 use lazy_static::lazy_static;
 use std::path::PathBuf;
 
 lazy_static! {
-    pub static ref PROOT: PathBuf = {
-        get_project_root().expect("Could not find project root")
-    };
-
     pub static ref PKGSTXT: PathBuf = PathBuf::from("/etc/rid/packages.txt");
     pub static ref META:    PathBuf = PathBuf::from("/etc/rid/meta");
     pub static ref UTILS:   PathBuf = PathBuf::from("/etc/rid/utils");
