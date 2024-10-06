561178e861189ce8f1d34a6dd0e3ea3cd83278ff
Tox <toxikuu@users.noreply.github.com>
2024-10-06 03:40:52 -0500

    added force flags

--- src/directions.rs
+++ src/directions.rs
@@ -4,18 +4,64 @@
 
 use crate::misc::exec;
 use crate::paths::UTILS;
+use crate::tracking::query_status;
+use crate::flags::FULL_FORCE;
+
+use crate::pr;
 
 pub fn eval_install_directions(pkg_str: &str) {
-    let command = format!("{}/meta-interface.sh i {}", UTILS.display(), pkg_str);
-    if let Err(e) = exec(&command) {
-        eprintln!("Failed to evaluate install directions: {}", e);
+    match query_status(pkg_str) {
+        Ok(status) => {
+            pr!(format!("Status: {}", status), 'v');
+            match status {
+                "installed" => {
+                    if !*FULL_FORCE.lock().unwrap() {
+                        pr!(format!("Package '{}' is already installed", pkg_str));
+                        return;
+                    } else {
+                        pr!(format!("Forcibly reinstalling package '{}'", pkg_str));
+                    }
+                },
+                "available" => {},
+                _ => {
+                    pr!(format!("Package '{}' unavailable", pkg_str));
+                    return;
+                },
+            }
+            let command = format!("{}/meta-interface.sh i {}", UTILS.display(), pkg_str);
+            if let Err(e) = exec(&command) {
+                eprintln!("Failed to evaluate install directions: {}", e);
+            }
+        },
+        Err(e) => eprintln!("Error querying package: {}", e),
     }
 }
 
 pub fn eval_removal_directions(pkg_str: &str) {
-    let command = format!("{}/meta-interface.sh r {}", UTILS.display(), pkg_str);
-    if let Err(e) = exec(&command) {
-        eprintln!("Failed to evaluate removal directions: {}", e);
+    match query_status(pkg_str) {
+        Ok(status) => {
+            pr!(format!("Status: {}", status), 'v');
+            match status {
+                "installed" => {},
+                "available" => {
+                    if !*FULL_FORCE.lock().unwrap() {
+                        pr!(format!("Package '{}' is not installed", pkg_str));
+                        return;
+                    } else {
+                        pr!(format!("Forcibly reinstalling package '{}'", pkg_str));
+                    }
+                },
+                _ => {
+                    pr!(format!("Package '{}' unavailable", pkg_str));
+                    return;
+                }
+            }
+            let command = format!("{}/meta-interface.sh r {}", UTILS.display(), pkg_str);
+            if let Err(e) = exec(&command) {
+                eprintln!("Failed to evaluate removal directions: {}", e);
+            }
+        },
+        Err(e) => eprintln!("Error querying package: {}", e),
     }
 }
 
