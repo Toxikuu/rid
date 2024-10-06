8a4af38e5053a2ad6bddf6e09024197e6dab3dee
Tox <toxikuu@users.noreply.github.com>
2024-10-06 03:41:59 -0500

    added build path

--- src/paths.rs
+++ src/paths.rs
@@ -4,6 +4,7 @@ use lazy_static::lazy_static;
 use std::path::PathBuf;
 
 lazy_static! {
+    pub static ref BUILD: PathBuf   = PathBuf::from("/tmp/rid/building");
     pub static ref PKGSTXT: PathBuf = PathBuf::from("/etc/rid/packages.txt");
     pub static ref META:    PathBuf = PathBuf::from("/etc/rid/meta");
     pub static ref UTILS:   PathBuf = PathBuf::from("/etc/rid/utils");
