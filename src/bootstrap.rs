b2869b6155bf72883517be79197fad0759c540c3
Tox <toxikuu@users.noreply.github.com>
2024-10-06 02:38:25 -0500

    added /tmp support

--- src/bootstrap.rs
+++ src/bootstrap.rs
@@ -1,23 +1,25 @@
 // src/bootstrap.rs
 //
 // Responsible for bootsrapping rid
-// Bootsrapping includes installing directories into /etc/rid and grabbing meta files
+// Bootsrapping includes creating directories and files
+// Plans exist to fetch build scripts as well
 
 use std::fs::{self, File};
 use std::path::Path;
 use std::io;
 use crate::paths::{SOURCES, META, UTILS, PKGSTXT};
 
+use crate::pr;
+
 fn mkdir<P: AsRef<Path>>(path: P) -> io::Result<()> {
     let path_ref = path.as_ref();
 
     if path_ref.exists() {
-        println!("Directory '{}' extant", path_ref.display());
+        pr!(format!("Directory '{}' extant", path_ref.display()), 'v');
     } else {
         fs::create_dir_all(path_ref)?;
-        println!("Created directory '{}'", path_ref.display());
+        pr!(format!("Created directory '{}'", path_ref.display()));
     }
-
     Ok(())
 }
 
@@ -25,13 +27,23 @@ fn touch<P: AsRef<Path>>(path: P) -> io::Result<()> {
     let path_ref = path.as_ref();
 
     if path_ref.exists() {
-        println!("File '{}' extant", path_ref.display());
+        pr!(format!("File '{}' extant", path_ref.display()), 'v');
     } else {
         let _file = File::create(path_ref)?;
-        println!("Created file '{}'", path_ref.display());
+        pr!(format!("Created file '{}'", path_ref.display()));
     }
-
     Ok(())
+}
+
+pub fn tmp() {
+    pr!("Attempting to create temp dirs...", 'v');
+    let dirs = ["/tmp/rid/building", "/tmp/rid/extraction"];
+
+    for dir in dirs.iter() {
+        if let Err(e) = mkdir(dir) {
+            eprintln!("Error creating directory: {}", e);
+        }
+    }
 }
 
 pub fn run() {
