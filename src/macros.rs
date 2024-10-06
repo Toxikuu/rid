25ce97400ecbc0af955d386950e60fd57dc4b02b
Tox <toxikuu@users.noreply.github.com>
2024-10-06 02:18:06 -0500

    pr! definition

---
+++ src/macros.rs
@@ -0,0 +1,38 @@
+// src/macros.rs
+
+// pr! usage:
+// pr!("hello") prints hello unless -q
+// pr!("hello", 'v') prints hello if -v
+// pr!("hello", 'q') prints hello despite -q
+//
+// note to self: avoid return in macros because it can cause unexpected behavior
+
+#[macro_export]
+macro_rules! pr {
+    ($fmt:expr) => {{
+        use $crate::flags::QUIET;
+
+        if !*QUIET.lock().unwrap() {
+            println!("{}", $fmt);
+        }
+    }};
+    ($fmt:expr, $flag:expr) => {{
+        use $crate::flags::{VERBOSE, QUIET};
+
+        match $flag {
+            'v' => {
+                if *VERBOSE.lock().unwrap() {
+                    println!("{}", $fmt);
+                }
+            },
+            'q' => {
+                println!("{}", $fmt);
+            },
+            _ => {
+                if !*QUIET.lock().unwrap() {
+                    println!("{}", $fmt);
+                }
+            }
+        }
+    }};
+}
