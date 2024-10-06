de6a7505cce55ed39839aefbf769928100a753e0
Tox <toxikuu@users.noreply.github.com>
2024-10-06 02:16:32 -0500

    removed fluff

--- src/directions.rs
+++ src/directions.rs
@@ -10,32 +10,18 @@ pub fn eval_install_directions(pkg_str: &str) {
     if let Err(e) = exec(&command) {
         eprintln!("Failed to evaluate install directions: {}", e);
     }
-    //match exec(&command) {
-    //    Ok(output) => println!("{}", output),
-    //    Err(e) => eprintln!("Failed to evaluate install directions: {}", e),
-    //}
 }
-
 
 pub fn eval_removal_directions(pkg_str: &str) {
     let command = format!("{}/meta-interface.sh r {}", UTILS.display(), pkg_str);
     if let Err(e) = exec(&command) {
         eprintln!("Failed to evaluate removal directions: {}", e);
     }
-    //match exec(&command) {
-    //    Ok(output) => println!("{}", output),
-    //    Err(e) => eprintln!("Failed to evaluate removal directions: {}", e),
-    //}
 }
-
 
 pub fn eval_update_directions(pkg_str: &str) {
     let command = format!("{}/meta-interface.sh u {}", UTILS.display(), pkg_str);
     if let Err(e) = exec(&command) {
         eprintln!("Failed to evaluate update directions: {}", e);
     }
-    //match exec(&command) {
-    //    Ok(output) => println!("{}", output),
-    //    Err(e) => eprintln!("Failed to evaluate update directions: {}", e),
-    //}
 }
