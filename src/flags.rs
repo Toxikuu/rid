9909d82e6d14ea9ae8af56a734bfe5f55684ac05
Tox <toxikuu@users.noreply.github.com>
2024-10-06 15:08:03 -0500

    adjusted flags

--- src/flags.rs
+++ src/flags.rs
@@ -6,13 +6,15 @@ use std::sync::Mutex;
 lazy_static! {
     pub static ref VERBOSE: Mutex<bool>    = Mutex::new(false);
     pub static ref QUIET: Mutex<bool>      = Mutex::new(false);
-    pub static ref FORCE: Mutex<bool>      = Mutex::new(false);
-    pub static ref FULL_FORCE: Mutex<bool> = Mutex::new(false);
+    pub static ref FORCE_DOWNLOAD: Mutex<bool>      = Mutex::new(false);
+    pub static ref FORCE_INSTALL: Mutex<bool> = Mutex::new(false);
+    pub static ref FORCE_REMOVE: Mutex<bool> = Mutex::new(false);
 }
 
-pub fn set_flags(verbose: bool, quiet: bool, force: bool, force_capital: bool) {
+pub fn set_flags(verbose: bool, quiet: bool, force_download: bool, force_install: bool, force_remove: bool) {
     *VERBOSE.lock().unwrap() = verbose;
     *QUIET.lock().unwrap() = quiet;
-    *FORCE.lock().unwrap() = force;
-    *FULL_FORCE.lock().unwrap() = force_capital;
+    *FORCE_DOWNLOAD.lock().unwrap() = force_download;
+    *FORCE_INSTALL.lock().unwrap() = force_install;
+    *FORCE_REMOVE.lock().unwrap() = force_remove;
 }
