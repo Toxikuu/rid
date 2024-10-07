3504f41ec0d8d6d51d5fef48d571e492759d1eee
Tox <toxikuu@users.noreply.github.com>
2024-10-07 16:39:05 -0500

    added /etc/rid path

--- src/paths.rs
+++ src/paths.rs
@@ -4,7 +4,8 @@ use lazy_static::lazy_static;
 use std::path::PathBuf;
 
 lazy_static! {
-    pub static ref BUILD:   PathBuf   = PathBuf::from("/tmp/rid/building");
+    pub static ref ETCRID:  PathBuf = PathBuf::from("/etc/rid");
+    pub static ref BUILD:   PathBuf = PathBuf::from("/tmp/rid/building");
     pub static ref PKGSTXT: PathBuf = PathBuf::from("/etc/rid/packages.txt");
     pub static ref META:    PathBuf = PathBuf::from("/etc/rid/meta");
     pub static ref RBIN:    PathBuf = PathBuf::from("/etc/rid/rbin");
