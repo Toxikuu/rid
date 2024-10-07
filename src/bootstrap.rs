97d4a890602afc0fbf02592d3d9ef1c73377e8ea
Tox <toxikuu@users.noreply.github.com>
2024-10-06 21:59:40 -0500

    improved bootstrapping

--- src/bootstrap.rs
+++ src/bootstrap.rs
@@ -4,13 +4,75 @@
 // Bootsrapping includes creating directories and files
 // Plans exist to fetch build scripts as well
 
+use reqwest::blocking::get;
+use std::error::Error;
+use std::process::exit;
 use std::fs::{self, File};
 use std::path::Path;
-use std::io;
+use std::io::{self, Write};
 use std::os::unix::fs::PermissionsExt;
 use crate::paths::{SOURCES, META, UTILS, PKGSTXT};
 
 use crate::pr;
+
+fn dl(url: &str, outdir: &str) -> Result<String, Box<dyn Error>> {
+    let file_name = url.split('/').last().ok_or("Invalid URL")?;
+    let file_path = Path::new(outdir).join(file_name);
+
+    let r = get(url)?;
+    if r.status().is_success() {
+        let mut file = File::create(&file_path)?;
+
+        let content = r.bytes()?;
+        file.write_all(&content)?;
+
+        Ok(file_name.to_string())
+    } else {
+        Err(format!("Failed to download file: HTTP status {}", r.status()).into())
+    }
+}
+
+fn get_rid() {
+    match dl(
+        "https://raw.githubusercontent.com/Toxikuu/rid/refs/heads/master/utils/meta-interface.sh",
+        "/etc/rid/utils"
+    ) {
+        Ok(_) => pr!("Downloaded meta-interface", 'v'),
+        Err(e) => {
+            eprintln!("Failed to download meta-interface.sh: {}", e);
+            exit(1);
+        }
+    }
+    match dl(
+        "https://raw.githubusercontent.com/Toxikuu/rid/refs/heads/master/meta/rid",
+        "/etc/rid/meta"
+        ) {
+        Ok(_) => {
+            pr!("Downloaded rid");
+            pr!("Now, run rid -i rid to finish bootstrapping");
+        },
+        Err(e) => {
+            eprintln!("Failed to download rid: {}", e);
+            exit(1);
+        }
+    }
+
+    match dl(
+        "https://raw.githubusercontent.com/Toxikuu/rid/refs/heads/master/env",
+        "/etc/rid/"
+        ) {
+        Ok(_) => {
+            pr!("Downloaded env", 'v');
+        },
+        Err(e) => {
+            eprintln!("Failed to download env: {}", e);
+            exit(1);
+        }
+    }
+
+    let permissions = fs::Permissions::from_mode(0o755);
+    fs::set_permissions("/etc/rid/utils/meta-interface.sh", permissions).unwrap();
+}
 
 fn mkdir<P: AsRef<Path>>(path: P) -> io::Result<()> {
     let path_ref = path.as_ref();
@@ -65,4 +127,7 @@ pub fn run() {
 
     let permissions = fs::Permissions::from_mode(0o666);
     fs::set_permissions(&*PKGSTXT, permissions).unwrap();
+
+
+    get_rid();
 }
