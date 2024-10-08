5066f8d6c346cf674219511faa8a62722ca8f9d6
Tox <toxikuu@users.noreply.github.com>
2024-10-07 21:29:11 -0500

    added tmp paths

--- src/paths.rs
+++ src/paths.rs
@@ -5,9 +5,14 @@ use std::path::PathBuf;
 
 lazy_static! {
     pub static ref ETCRID:  PathBuf = PathBuf::from("/etc/rid");
-    pub static ref BUILD:   PathBuf = PathBuf::from("/tmp/rid/building");
     pub static ref PKGSTXT: PathBuf = PathBuf::from("/etc/rid/packages.txt");
     pub static ref META:    PathBuf = PathBuf::from("/etc/rid/meta");
     pub static ref RBIN:    PathBuf = PathBuf::from("/etc/rid/rbin");
     pub static ref SOURCES: PathBuf = PathBuf::from("/etc/rid/sources");
+
+    pub static ref TMPRID:   PathBuf = PathBuf::from("/tmp/rid/");
+    pub static ref BUILDING:   PathBuf = PathBuf::from("/tmp/rid/building");
+    pub static ref EXTRACTION:   PathBuf = PathBuf::from("/tmp/rid/extraction");
+    pub static ref TRASH:   PathBuf = PathBuf::from("/tmp/rid/trash");
+    pub static ref DEST:   PathBuf = PathBuf::from("/tmp/rid/dest");
 }
