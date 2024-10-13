8a7d4b64b15d05403e6c4cad9798e12d8e663f46
Tox <toxikuu@users.noreply.github.com>
2024-10-12 23:26:18 -0500

    formatting and prune

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 // src/main.rs
 
+use crate::paths::PKGSTXT;
 use clap::Parser;
 use directions::{eval_install_directions, eval_removal_directions, eval_update_directions};
 use misc::check_perms;
-use crate::paths::PKGSTXT;
 
-mod package;
-mod tracking;
-mod paths;
-mod misc;
-mod fetch;
-mod directions;
-mod resolvedeps;
 mod bootstrap;
+mod directions;
+mod fetch;
 mod flags;
 mod macros;
+mod misc;
+mod package;
+mod paths;
+mod resolvedeps;
+mod tracking;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -26,7 +26,6 @@ struct Args {
     install_no_deps: Option<Vec<String>>,
 
     #[arg(short = 'r', long, value_name = "PACKAGE", value_parser, num_args = 1.., value_delimiter = ' ')]
-
     remove: Option<Vec<String>>,
 
     #[arg(short = 'u', long, value_name = "PACKAGE", value_parser, num_args = 1.., value_delimiter = ' ')]
@@ -34,7 +33,7 @@ struct Args {
 
     #[arg(short = 'd', long, value_name = "PACKAGE", value_parser, num_args = 1.., value_delimiter = ' ')]
     dependencies: Option<Vec<String>>,
-    
+
     // function flags
     #[arg(short = 'l', long)]
     list: bool,
@@ -65,8 +64,13 @@ struct Args {
 fn main() {
     let args = Args::parse();
     flags::set_flags(args.verbose, args.quiet, args.download, args.force);
-    pr!(format!("Flags: verbose={}, quiet={}, download={}, force={}", 
-                args.verbose, args.quiet, args.download, args.force), 'v');
+    pr!(
+        format!(
+            "Flags: verbose={}, quiet={}, download={}, force={}",
+            args.verbose, args.quiet, args.download, args.force
+        ),
+        'v'
+    );
 
     bootstrap::tmp();
     //let _ = misc::exec("sleep 5");
@@ -77,7 +81,10 @@ fn main() {
     let _ = tracking::alphabetize();
 
     match args {
-        Args { install_no_deps: Some(pkgs), .. } => {
+        Args {
+            install_no_deps: Some(pkgs),
+            ..
+        } => {
             check_perms();
 
             for pkg in pkgs {
@@ -88,12 +95,15 @@ fn main() {
                         fetch::wrap(&pkg_);
                         eval_install_directions(&pkg);
                         tracking::add_package(&pkg, &pkg_.version);
-                    },
+                    }
                     Err(e) => eprintln!("Failed to form package '{}': {}", pkg, e),
                 }
             }
         }
-        Args { install: Some(pkgs), .. } => {
+        Args {
+            install: Some(pkgs),
+            ..
+        } => {
             check_perms();
 
             for pkg in pkgs {
@@ -112,18 +122,20 @@ fn main() {
                                     fetch::wrap(&dep_);
                                     eval_install_directions(&dep);
                                     tracking::add_package(&dep, &dep_.version);
-                                },
+                                }
                                 Err(e) => eprintln!("Failed to form package '{}': {}", dep, e),
                             }
                         }
-                    },
+                    }
                     Err(e) => {
                         eprintln!("Failed to form package '{}': {}", pkg, e);
-                    },
+                    }
                 }
             }
         }
-        Args { remove: Some(pkgs), .. } => {
+        Args {
+            remove: Some(pkgs), ..
+        } => {
             check_perms();
 
             for pkg in pkgs {
@@ -132,7 +144,9 @@ fn main() {
                 tracking::remove_package(&pkg);
             }
         }
-        Args { update: Some(pkgs), .. } => {
+        Args {
+            update: Some(pkgs), ..
+        } => {
             check_perms();
 
             for pkg in pkgs {
@@ -143,13 +157,15 @@ fn main() {
                         fetch::wrap(&pkg_);
                         eval_update_directions(&pkg);
                         tracking::add_package(&pkg, &pkg_.version);
-                    },
+                    }
                     Err(e) => eprintln!("Failed to form package '{}': {}", pkg, e),
                 }
             }
         }
-        Args { dependencies: Some(pkgs), .. } => {
-
+        Args {
+            dependencies: Some(pkgs),
+            ..
+        } => {
             for pkg in pkgs {
                 pr!(format!("Dependencies for {}:", pkg));
 
@@ -159,36 +175,34 @@ fn main() {
                         for dep in dependencies {
                             pr!(format!(" - {}", dep), 'q');
                         }
-                    },
+                    }
                     Err(e) => {
                         eprintln!("Failed to form package '{}': {}", pkg, e);
-                    },
+                    }
                 }
             }
         }
-        Args { list, .. } if list => {
-            match misc::read_file(PKGSTXT.clone()) {
-                Ok(contents) => {
-                    pr!("\x1b[30;1;3mPACKAGES\x1b[0m");
-                    for line in contents.lines() {
-                        let formatted_line = misc::format_line(line);
-                        pr!(format!("  {}", formatted_line), 'q');
-                    }
-                },
-                Err(e) => eprintln!("Error reading file: {}", e),
+        Args { list, .. } if list => match misc::read_file(PKGSTXT.clone()) {
+            Ok(contents) => {
+                pr!("\x1b[30;1;3mPACKAGES\x1b[0m");
+                for line in contents.lines() {
+                    let formatted_line = misc::format_line(line);
+                    pr!(format!("  {}", formatted_line), 'q');
+                }
             }
-        }
-        Args { bootstrap, ..} if bootstrap => {
+            Err(e) => eprintln!("Error reading file: {}", e),
+        },
+        Args { bootstrap, .. } if bootstrap => {
             check_perms();
             pr!("\x1b[36;1mBootstrapping rid...\x1b[0m");
             bootstrap::run();
         }
-        Args { sync, ..} if sync => {
+        Args { sync, .. } if sync => {
             check_perms();
             pr!("\x1b[36;1mSyncing rid-meta...\x1b[0m");
             bootstrap::get_rid_meta(false);
         }
-        Args { sync_overwrite, ..} if sync_overwrite => {
+        Args { sync_overwrite, .. } if sync_overwrite => {
             check_perms();
             pr!("\x1b[36;1mSyncing rid-meta with overwrite...\x1b[0m");
             bootstrap::get_rid_meta(true);
