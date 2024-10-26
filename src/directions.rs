e7db8dbe485c2f012105822eb6a1d2911da9997e
Tox <toxikuu@users.noreply.github.com>
2024-10-26 14:27:31 -0500

    major code simplification

--- src/directions.rs
+++ src/directions.rs
@@ -2,71 +2,21 @@
 //
 // responsible for executing various build directions
 
-use crate::flags::FORCE;
+use crate::erm;
 use crate::misc::exec;
-use crate::package::PackageStatus;
 use crate::paths::RBIN;
-use crate::tracking::query_status;
-
-use crate::{erm, pr};
 
 pub fn eval_install_directions(pkg_str: &str) {
-    match query_status(pkg_str) {
-        Ok(status) => {
-            pr!(format!("Status: {:?}", status), 'v');
-
-            match status {
-                PackageStatus::Installed => {
-                    if !*FORCE.lock().unwrap() {
-                        pr!(format!("Package '{}' is already installed", pkg_str));
-                        return;
-                    } else {
-                        pr!(format!("Forcibly installing package '{}'", pkg_str), 'v');
-                    }
-                }
-                PackageStatus::Available => {}
-                PackageStatus::Removed => {
-                    pr!(format!(
-                        "Package '{}' has been removed. Reinstalling.",
-                        pkg_str
-                    ));
-                }
-            }
-
-            let command = format!("{}/mint i {}", RBIN.display(), pkg_str);
-            if let Err(e) = exec(&command) {
-                erm!("Failed to evaluate install directions: {}", e);
-            }
-        }
-        Err(e) => erm!("Failed to query package status: {}", e),
+    let command = format!("{}/mint i {}", RBIN.display(), pkg_str);
+    if let Err(e) = exec(&command) {
+        erm!("Failed to evaluate install directions: {}", e);
     }
 }
 
 pub fn eval_removal_directions(pkg_str: &str) {
-    match query_status(pkg_str) {
-        Ok(status) => {
-            pr!(format!("Status: {:?}", status), 'v');
-            match status {
-                PackageStatus::Installed => {}
-                PackageStatus::Available => {
-                    if !*FORCE.lock().unwrap() {
-                        pr!(format!("Package '{}' is not installed", pkg_str));
-                        return;
-                    } else {
-                        pr!(format!("Forcibly removing package '{}'", pkg_str), 'v');
-                    }
-                }
-                _ => {
-                    pr!(format!("Package '{}' unavailable", pkg_str));
-                    return;
-                }
-            }
-            let command = format!("{}/mint r {}", RBIN.display(), pkg_str);
-            if let Err(e) = exec(&command) {
-                erm!("Failed to evaluate removal directions: {}", e);
-            }
-        }
-        Err(e) => erm!("Error querying package: {}", e),
+    let command = format!("{}/mint r {}", RBIN.display(), pkg_str);
+    if let Err(e) = exec(&command) {
+        erm!("Failed to evaluate removal directions: {}", e);
     }
 }
 
