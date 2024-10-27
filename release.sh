eeb79a71f5f1e0c92bdcb5a5334812cdcb2aeb6f
Tox <toxikuu@users.noreply.github.com>
2024-10-26 19:05:39 -0500

    improvements

--- release.sh
+++ release.sh
@@ -4,20 +4,27 @@ set -e # exit on error
 
 [ -f src/main.rs ] || {
     echo "Guessing project root: /code/rid"
-    pushd /code/rid
+    pushd /code/rid > /dev/null
 }
 
+pushd target/release
 XZ_OPT="-9e"
 
-echo "Packaging rid-offline"
-cargo build --release --no-default-features --features=offline
-pushd target/release
-mv -vf rid{,-offline}
-tar cJf rid-offline.tar.xz rid-offline
+echo -e "\x1b[36;1m  Removing old releases...\x1b[0m"
+rm -vf rid*
 
-echo "Packaging rid"
+echo -e "\x1b[36;1m  Packaging rid-offline...\x1b[0m"
+cargo build --release --no-default-features --features=offline
+mv -vf rid{,-offline}
+tar cJvf rid-offline.tar.xz rid-offline
+
+echo -e "\x1b[36;1m  Packaging rid...\x1b[0m"
 cargo build --release
-tar cJf rid.tar.xz rid
+tar cJvf rid.tar.xz rid
+
+echo -e "\x1b[36;1m  Packaging project root...\x1b[0m"
+tar cJvf rid-root.tar.xz ../../rbin ../../env
 
 popd > /dev/null
 popd > /dev/null 2>&1 || true # in case project root was guessed
+echo -e "\x1b[36;1m  Done!\x1b[0m"
