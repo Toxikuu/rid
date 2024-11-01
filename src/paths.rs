7c3a81303dee349431974603dae92454223b88ac
Tox <toxikuu@users.noreply.github.com>
2024-11-01 14:21:33 -0500

    made env vars crash

--- src/paths.rs
+++ src/paths.rs
@@ -7,25 +7,18 @@ use std::env;
 use std::path::PathBuf;
 
 lazy_static! {
-    pub static ref RIDHOME: PathBuf =
-        PathBuf::from(env::var("RIDHOME").unwrap_or_else(|_| "/etc/rid".to_string()));
+    pub static ref RIDHOME: PathBuf = PathBuf::from(env::var("RIDHOME").expect("Set $RIDHOME"));
     pub static ref PKGSJSON: PathBuf =
-        PathBuf::from(env::var("RIDPKGSJSON").unwrap_or_else(|_| "/etc/rid/pkgs.json".to_string()));
-    pub static ref META: PathBuf =
-        PathBuf::from(env::var("RIDMETA").unwrap_or_else(|_| "/etc/rid/meta".to_string()));
-    pub static ref RBIN: PathBuf =
-        PathBuf::from(env::var("RIDBIN").unwrap_or_else(|_| "/etc/rid/bin".to_string()));
+        PathBuf::from(env::var("RIDPKGSJSON").expect("Set $RIDPKGSJSON"));
+    pub static ref META: PathBuf = PathBuf::from(env::var("RIDMETA").expect("Set $RIDMETA"));
+    pub static ref RBIN: PathBuf = PathBuf::from(env::var("RIDBIN").expect("Set $RIDBIN"));
     pub static ref SOURCES: PathBuf =
-        PathBuf::from(env::var("RIDSOURCES").unwrap_or_else(|_| "/etc/rid/sources".to_string()));
-    pub static ref TMPRID: PathBuf =
-        PathBuf::from(env::var("RIDTMP").unwrap_or_else(|_| "/tmp/rid/".to_string()));
+        PathBuf::from(env::var("RIDSOURCES").expect("Set $RIDSOURCES"));
+    pub static ref TMPRID: PathBuf = PathBuf::from(env::var("RIDTMP").expect("Set $RIDTMP"));
     pub static ref BUILDING: PathBuf =
-        PathBuf::from(env::var("RIDBUILDING").unwrap_or_else(|_| "/tmp/rid/building".to_string()));
-    pub static ref EXTRACTION: PathBuf = PathBuf::from(
-        env::var("RIDEXTRACTION").unwrap_or_else(|_| "/tmp/rid/extraction".to_string())
-    );
-    pub static ref TRASH: PathBuf =
-        PathBuf::from(env::var("RIDTRASH").unwrap_or_else(|_| "/tmp/rid/trash".to_string()));
-    pub static ref DEST: PathBuf =
-        PathBuf::from(env::var("RIDDEST").unwrap_or_else(|_| "/tmp/rid/dest".to_string()));
+        PathBuf::from(env::var("RIDBUILDING").expect("Set $RIDBUILDING"));
+    pub static ref EXTRACTION: PathBuf =
+        PathBuf::from(env::var("RIDEXTRACTION").expect("Set $RIDEXTRACTION"));
+    pub static ref TRASH: PathBuf = PathBuf::from(env::var("RIDTRASH").expect("Set $RIDTRASH"));
+    pub static ref DEST: PathBuf = PathBuf::from(env::var("RIDDEST").expect("Set $RIDDEST"));
 }
