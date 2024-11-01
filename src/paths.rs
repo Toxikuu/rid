a54b4ac83dc1260f44705eb411465be76bfd3ebf
Tox <toxikuu@users.noreply.github.com>
2024-11-01 13:14:57 -0500

    reduced collision likelihood for environment variables

--- src/paths.rs
+++ src/paths.rs
@@ -10,21 +10,22 @@ lazy_static! {
     pub static ref RIDHOME: PathBuf =
         PathBuf::from(env::var("RIDHOME").unwrap_or_else(|_| "/etc/rid".to_string()));
     pub static ref PKGSJSON: PathBuf =
-        PathBuf::from(env::var("PKGSJSON").unwrap_or_else(|_| "/etc/rid/pkgs.json".to_string()));
+        PathBuf::from(env::var("RIDPKGSJSON").unwrap_or_else(|_| "/etc/rid/pkgs.json".to_string()));
     pub static ref META: PathBuf =
-        PathBuf::from(env::var("META").unwrap_or_else(|_| "/etc/rid/meta".to_string()));
+        PathBuf::from(env::var("RIDMETA").unwrap_or_else(|_| "/etc/rid/meta".to_string()));
     pub static ref RBIN: PathBuf =
-        PathBuf::from(env::var("RBIN").unwrap_or_else(|_| "/etc/rid/rbin".to_string()));
+        PathBuf::from(env::var("RIDBIN").unwrap_or_else(|_| "/etc/rid/rbin".to_string()));
     pub static ref SOURCES: PathBuf =
-        PathBuf::from(env::var("SOURCES").unwrap_or_else(|_| "/etc/rid/sources".to_string()));
+        PathBuf::from(env::var("RIDSOURCES").unwrap_or_else(|_| "/etc/rid/sources".to_string()));
     pub static ref TMPRID: PathBuf =
-        PathBuf::from(env::var("TMPRID").unwrap_or_else(|_| "/tmp/rid/".to_string()));
+        PathBuf::from(env::var("RIDTMP").unwrap_or_else(|_| "/tmp/rid/".to_string()));
     pub static ref BUILDING: PathBuf =
-        PathBuf::from(env::var("BUILDING").unwrap_or_else(|_| "/tmp/rid/building".to_string()));
-    pub static ref EXTRACTION: PathBuf =
-        PathBuf::from(env::var("EXTRACTION").unwrap_or_else(|_| "/tmp/rid/extraction".to_string()));
+        PathBuf::from(env::var("RIDBUILDING").unwrap_or_else(|_| "/tmp/rid/building".to_string()));
+    pub static ref EXTRACTION: PathBuf = PathBuf::from(
+        env::var("RIDEXTRACTION").unwrap_or_else(|_| "/tmp/rid/extraction".to_string())
+    );
     pub static ref TRASH: PathBuf =
-        PathBuf::from(env::var("TRASH").unwrap_or_else(|_| "/tmp/rid/trash".to_string()));
+        PathBuf::from(env::var("RIDTRASH").unwrap_or_else(|_| "/tmp/rid/trash".to_string()));
     pub static ref DEST: PathBuf =
-        PathBuf::from(env::var("DEST").unwrap_or_else(|_| "/tmp/rid/dest".to_string()));
+        PathBuf::from(env::var("RIDDEST").unwrap_or_else(|_| "/tmp/rid/dest".to_string()));
 }
