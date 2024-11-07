7ea20c3182ed1a8fa0b52d2bd613dfb32dc0c17a
Tox <toxikuu@users.noreply.github.com>
2024-11-06 22:01:29 -0600

    major refactoring

--- src/directions.rs
+++ src/directions.rs
@@ -2,27 +2,13 @@
 //
 // responsible for executing various build directions
 
-use crate::erm;
+use crate::die;
 use crate::misc::exec;
-use crate::paths::RBIN;
+use crate::paths::BIN;
 
-pub fn eval_install_directions(pkg_str: &str) {
-    let command = format!("{}/mint i {}", RBIN.display(), pkg_str);
+pub fn eval_action(a: char, pkg_str: &str) {
+    let command = format!("{}/mint {} {}", BIN.display(), a, pkg_str);
     if let Err(e) = exec(&command) {
-        erm!("Failed to evaluate install directions: {}", e);
-    }
-}
-
-pub fn eval_removal_directions(pkg_str: &str) {
-    let command = format!("{}/mint r {}", RBIN.display(), pkg_str);
-    if let Err(e) = exec(&command) {
-        erm!("Failed to evaluate removal directions: {}", e);
-    }
-}
-
-pub fn eval_update_directions(pkg_str: &str) {
-    let command = format!("{}/mint u {}", RBIN.display(), pkg_str);
-    if let Err(e) = exec(&command) {
-        erm!("Failed to evaluate update directions: {}", e);
+        die!("Failed to evaluate action '{}': {}", a, e)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 // src/main.rs
 
-use crate::flags::FORCE;
 use crate::paths::PKGSJSON;
 use clap::Parser;
-use directions::{eval_install_directions, eval_removal_directions, eval_update_directions};
-use misc::check_perms;
-use package::{form_package, PackageStatus};
-use std::process::exit;
 
+mod sets;
+mod args;
 mod bootstrap;
 mod clean;
 mod directions;
@@ -20,30 +17,37 @@ mod paths;
 mod resolvedeps;
 mod tracking;
 
+// TODO: Add color to #[command()]
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[command(
+    version,
+    about = "Source-based LFS package manager",
+    long_about,
+    arg_required_else_help = true,
+    after_help = "If you have any questions, you can DM me on Discord @toxikuu"
+)]
 struct Args {
-    #[arg(short = 'i', long, value_name = "PACKAGE", value_parser, num_args = 1.., value_delimiter = ' ')]
+    #[arg(short = 'i', long, value_name = "PACKAGE", num_args = 1.., value_delimiter = ' ')]
     install: Option<Vec<String>>,
 
-    #[arg(short = 'n', long, value_name = "PACKAGE", value_parser, num_args = 1.., value_delimiter = ' ')]
+    #[arg(short = 'n', long, value_name = "PACKAGE", num_args = 1.., value_delimiter = ' ')]
     install_no_deps: Option<Vec<String>>,
 
-    #[arg(short = 'r', long, value_name = "PACKAGE", value_parser, num_args = 1.., value_delimiter = ' ')]
+    #[arg(short = 'r', long, value_name = "PACKAGE", num_args = 1.., value_delimiter = ' ')]
     remove: Option<Vec<String>>,
 
-    #[arg(short = 'u', long, value_name = "PACKAGE", value_parser, num_args = 1.., value_delimiter = ' ')]
+    #[arg(short = 'u', long, value_name = "PACKAGE", num_args = 1.., value_delimiter = ' ')]
     update: Option<Vec<String>>,
 
-    #[arg(short = 'd', long, value_name = "PACKAGE", value_parser, num_args = 1.., value_delimiter = ' ')]
+    #[arg(short = 'd', long, value_name = "PACKAGE", num_args = 1.., value_delimiter = ' ')]
     dependencies: Option<Vec<String>>,
 
-    #[arg(short = 'p', long, value_name = "PACKAGE", value_parser, num_args = 1.., value_delimiter = ' ')]
+    #[arg(short = 'p', long, value_name = "PACKAGE", num_args = 1.., value_delimiter = ' ')]
     prune: Option<Vec<String>>,
 
     // function flags
-    #[arg(short = 'l', long)]
-    list: bool,
+    #[arg(short = 'l', long, value_name = "PACKAGE", num_args = 0.., value_delimiter = ' ')]
+    list: Option<Vec<String>>, // TODO: rewrite this without Option<>
 
     #[arg(short = 'b', long)]
     bootstrap: bool,
@@ -73,7 +77,6 @@ struct Args {
 
 fn main() {
     let mut args = Args::parse();
-    let mut matched = false;
 
     if args.update.is_some() {
         args.force = true;
@@ -89,277 +92,50 @@ fn main() {
     );
 
     bootstrap::tmp();
-    let mut pkg_list =
-        tracking::load_package_list(PKGSJSON.as_path()).unwrap_or_else(|_| Vec::new());
+    let mut pkg_list = tracking::load_package_list(PKGSJSON.as_path()).unwrap_or_else(|_| Vec::new());
     let _ = tracking::append_json(&mut pkg_list); // appends any new metafiles to the json
 
     if args.bootstrap {
-        #[cfg(feature = "offline")]
-        {
-            erm!("Bootstrapping is not supported for offline rid");
-            exit(1)
-        }
-
-        #[cfg(not(feature = "offline"))]
-        {
-            matched = true;
-            check_perms();
-            msg!("Bootstrapping rid...");
-            bootstrap::run();
-        }
+        args::bootstrap();
     }
 
     if args.cache {
-        matched = true;
-        let _ = tracking::populate_json();
+        args::cache();
     }
 
     if args.sync {
-        #[cfg(feature = "offline")]
-        {
-            erm!("Syncing is not supported for offline rid");
-            exit(1)
-        }
-
-        #[cfg(not(feature = "offline"))]
-        {
-            matched = true;
-            check_perms();
-            msg!("Syncing rid-meta...");
-            bootstrap::get_rid_meta(false);
-        }
+        args::sync();
     }
 
     if args.sync_overwrite {
-        #[cfg(feature = "offline")]
-        {
-            erm!("Syncing is not supported for offline rid");
-            exit(1)
-        }
-
-        #[cfg(not(feature = "offline"))]
-        {
-            matched = true;
-            check_perms();
-            msg!("Syncing rid-meta with overwrite...");
-            bootstrap::get_rid_meta(true);
-        }
+        args::sync_overwrite();
     }
 
-    if args.list {
-        matched = true;
-        match misc::read_json(PKGSJSON.clone()) {
-            Ok(package_list) => {
-                msg!("PACKAGES");
-                for p in package_list {
-                    let line = format!("{}={} ~ {:?}", p.name, p.version, p.status,);
-
-                    let formatted_line = misc::format_line(&line, 32);
-                    println!("  {}", formatted_line);
-                }
-            }
-            Err(e) => erm!("Errror reading pkgs.json: {}", e),
-        }
+    if let Some(pkgs) = args.list {
+        args::list(pkgs);
     }
 
     if let Some(pkgs) = args.remove {
-        matched = true;
-        check_perms();
-        for pkg in pkgs {
-            msg!("Removing {}", pkg);
-            eval_removal_directions(&pkg);
-            match tracking::remove_package(&mut pkg_list, &pkg) {
-                Ok(_) => msg!("Removed {}", pkg),
-                Err(e) => {
-                    erm!("Failed to track package '{}': {}", pkg, e);
-                }
-            }
-            clean::remove_tarballs(&pkg);
-        }
+        args::remove(pkgs, &mut pkg_list);
     }
 
     if let Some(pkgs) = args.prune {
-        matched = true;
-        check_perms();
-
-        for pkg in pkgs {
-            msg!("Pruning {}", pkg);
-            match form_package(&pkg) {
-                Ok(p) => {
-                    let num_removed = clean::prune_sources(&p);
-                    msg!("Pruned {} tarballs for {}", num_removed, pkg);
-                }
-                Err(e) => erm!("{}", e),
-            }
-        }
+        args::prune(pkgs);
     }
 
     if let Some(pkgs) = args.install_no_deps {
-        matched = true;
-        check_perms();
-
-        for pkg in pkgs {
-            match package::form_package(&pkg) {
-                Ok(p) => {
-                    let mut do_install = false;
-
-                    match &p.status {
-                        PackageStatus::Installed => {
-                            msg!("{}-{} is already installed", p.name, p.version);
-                            if *FORCE.lock().unwrap() {
-                                do_install = true
-                            };
-                        }
-                        _ => {
-                            msg!("Installing {}-{} without dependencies", p.name, p.version);
-                            do_install = true;
-                        }
-                    }
-
-                    vpr!("do_install = {}", do_install);
-                    if do_install {
-                        fetch::wrap(&p);
-                        eval_install_directions(&p.name);
-                        match tracking::add_package(&mut pkg_list, &p) {
-                            Ok(_) => msg!("Installed {}-{}", p.name, p.version),
-                            Err(e) => {
-                                erm!("Failed to track package '{}': {}", pkg, e);
-                                exit(1);
-                            }
-                        }
-                    }
-                }
-                Err(e) => erm!("{}", e),
-            }
-        }
+        args::install_no_deps(pkgs, &mut pkg_list);
     }
 
     if let Some(pkgs) = args.install {
-        matched = true;
-        check_perms();
-
-        for pkg in pkgs {
-            match package::form_package(&pkg) {
-                Ok(p) => {
-                    let deps = resolvedeps::resolve_deps(&p);
-                    for dep in &deps {
-                        vpr!("Dependency: {}", dep);
-                    }
-
-                    // i may want to add a function for displaying dependencies and share it
-                    // between --dependencies and here
-
-                    for dep in deps {
-                        match package::form_package(&dep) {
-                            Ok(d) => {
-                                let mut do_install = false;
-
-                                match &d.status {
-                                    PackageStatus::Installed => {
-                                        msg!("{}-{} is already installed", d.name, d.version);
-                                        if *FORCE.lock().unwrap() {
-                                            do_install = true
-                                        };
-                                    }
-                                    _ => {
-                                        msg!("Installing {}-{}", d.name, d.version);
-                                        do_install = true;
-                                    }
-                                }
-
-                                vpr!("do_install = {}", do_install);
-                                if do_install {
-                                    fetch::wrap(&d);
-                                    eval_install_directions(&dep);
-                                    match tracking::add_package(&mut pkg_list, &d) {
-                                        Ok(_) => msg!("Installed {}-{}", d.name, d.version),
-                                        Err(e) => {
-                                            erm!("Failed to track package '{}': {}", dep, e)
-                                        }
-                                    }
-                                }
-                            }
-                            Err(e) => erm!("{}", e),
-                        }
-                    }
-                }
-                Err(e) => {
-                    erm!("{}", e);
-                }
-            }
-        }
+        args::install(pkgs, &mut pkg_list);
     }
 
     if let Some(pkgs) = args.update {
-        matched = true;
-        check_perms();
-
-        for pkg in pkgs {
-            msg!("Updating {}", pkg);
-
-            match package::form_package(&pkg) {
-                Ok(p) => {
-                    fetch::wrap(&p);
-                    eval_update_directions(&p.name);
-                    match tracking::add_package(&mut pkg_list, &p) {
-                        Ok(_) => msg!("Updated to {}-{}", p.name, p.version),
-                        Err(e) => {
-                            erm!("Failed to track package '{}': {}", pkg, e);
-                        }
-                    }
-                }
-                Err(e) => erm!("{}", e),
-            }
-        }
+        args::update(pkgs, &mut pkg_list);
     }
 
     if let Some(pkgs) = args.dependencies {
-        matched = true;
-        for pkg in pkgs {
-            msg!("Dependencies for {}:", pkg);
-
-            match package::form_package(&pkg) {
-                Ok(p) => {
-                    let deps = resolvedeps::resolve_deps(&p);
-
-                    match misc::read_json(PKGSJSON.clone()) {
-                        Ok(package_list) => {
-                            let mut matches: Vec<String> = Vec::new();
-
-                            for dep in &deps {
-                                if dep.is_empty() {
-                                    erm!("Undefined dependency detected!");
-                                    std::process::exit(1);
-                                }
-
-                                for package in &package_list {
-                                    if package.name == *dep {
-                                        matches.push(format!(
-                                            "{}={} ~ {:?}",
-                                            package.name, package.version, package.status
-                                        ))
-                                    }
-                                }
-                            }
-
-                            for m in matches {
-                                let formatted_m = misc::format_line(&m, 30);
-                                println!("  {}", formatted_m)
-                            }
-                        }
-                        Err(e) => {
-                            erm!("Error reading packages.json: {}", e);
-                        }
-                    }
-                }
-                Err(e) => {
-                    erm!("{}", e);
-                }
-            }
-        }
-    }
-
-    if !matched {
-        erm!("No valid arguments provided")
+        args::dependencies(pkgs);
     }
 }
