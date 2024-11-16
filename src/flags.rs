b7984ed3fde604820358f43c6cae13ea29976edf
Tox <toxikuu@users.noreply.github.com>
2024-11-15 23:35:58 -0600

    removed download and yes flags

--- src/flags.rs
+++ src/flags.rs
@@ -8,13 +8,11 @@ use std::sync::Mutex;
 lazy_static! {
     pub static ref VERBOSE: Mutex<bool> = Mutex::new(false);
     pub static ref QUIET: Mutex<bool> = Mutex::new(false);
-    pub static ref DOWNLOAD: Mutex<bool> = Mutex::new(false);
     pub static ref FORCE: Mutex<bool> = Mutex::new(false);
 }
 
-pub fn set_flags(verbose: bool, quiet: bool, download: bool, force: bool) {
+pub fn set_flags(verbose: bool, quiet: bool, force: bool) {
     *VERBOSE.lock().unwrap() = verbose;
     *QUIET.lock().unwrap() = quiet;
-    *DOWNLOAD.lock().unwrap() = download;
     *FORCE.lock().unwrap() = force;
 }
