00009f3c2a9df75ddac89621aa0aae94571e94a4
Tox <toxikuu@users.noreply.github.com>
2024-11-15 23:40:08 -0600

    enforced switching to root and login shell in rid.sh

--- rid.sh
+++ rid.sh
@@ -14,4 +14,24 @@ export RIDPKGSJSON="$RIDHOME/pkgs.json"
 export RIDSOURCES="$RIDHOME/sources"
 export RIDSETS="$RIDHOME/sets"
 
+if [[ $EUID -ne 0 || ! $(shopt -q login_shell; echo $?) -eq 0 ]]; then
+  CMD=$(command -v sudo > /dev/null 2>&1 && echo "sudo su - root" || echo "su - root")
+  exec $CMD -c "
+    export RIDTMP=\$RIDTMP;
+    export RIDTRASH=\$RIDTRASH;
+    export RIDBUILDING=\$RIDBUILDING;
+    export RIDEXTRACTION=\$RIDEXTRACTION;
+    export RIDDEST=\$RIDDEST;
+    export RIDFAILED=\$RIDFAILED;
+    export RIDHOME=\$RIDHOME;
+    export RIDMETA=\$RIDMETA;
+    export RIDBIN=\$RIDBIN;
+    export RIDPKGSJSON=\$RIDPKGSJSON;
+    export RIDSOURCES=\$RIDSOURCES;
+    export RIDSETS=\$RIDSETS;
+    rid $*
+  "
+  exit 0
+fi
+
 rid "$@"
