66eb7f1be45b3cd5fc742fb4998083092a7f47a0
Tox <toxikuu@users.noreply.github.com>
2024-11-17 23:47:25 -0600

    improved release script

--- release.sh
+++ release.sh
@@ -1,36 +1,31 @@
 #!/bin/bash
 
-set -e # exit on error
+set -e
 
 [ -f src/main.rs ] || {
     echo "Guessing project root: /code/rid"
     pushd /code/rid > /dev/null
 }
 
-pushd target/release
+
+echo -e "\x1b[36;1m  Fixing version in README...\x1b[0m"
+RIDVERS=$(rg -oN '\d*\.\d*\.\d*\d' Cargo.toml | head -n1)
+sed -i "s/[0-9]*\.[0-9]*\.[0-9]*[0-9]/$RIDVERS/" README.md
+
+pushd target/release > /dev/null
 XZ_OPT="-9e"
 
 echo -e "\x1b[36;1m  Removing old releases...\x1b[0m"
 rm -vf rid*
 
-# rid-offline is now deprecated
-# echo -e "\x1b[36;1m  Packaging rid-offline...\x1b[0m"
-# cargo build --release --no-default-features --features=offline
-# mv -vf rid{,-offline}
-# echo "Compressing rid-offline"
-# tar cJvf rid-offline.tar.xz rid-offline > /dev/null
-# echo "Done"
-
 echo -e "\x1b[36;1m  Packaging rid...\x1b[0m"
 cargo build --release
 echo "Compressing rid"
 tar cJvf rid.tar.xz rid > /dev/null
-echo "Done"
 
 echo -e "\x1b[36;1m  Packaging project root...\x1b[0m"
 echo "Compressing rid-root"
 tar cJvf rid-root.tar.xz ../../bin ../../sets ../../env ../../rid.sh ../../{README,DOCS}.md > /dev/null 2>&1 # suppress a tar warning about leading ../
-echo "Done"
 
 popd > /dev/null
 popd > /dev/null 2>&1 || true # in case project root was guessed
