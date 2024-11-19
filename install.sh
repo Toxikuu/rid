8754cc467da82691c0ffc2d979d319c932cd5d0d
Tox <toxikuu@users.noreply.github.com>
2024-11-18 21:29:52 -0600

    used sudo instead of running as root

--- install.sh
+++ install.sh
@@ -6,42 +6,41 @@ set -e
 pushd . >/dev/null
 [ -e "$RIDENV"       ]  &&  . "$RIDENV"
 
-[ "$EUID" -ne 0      ]  &&  { echo "Insufficient permissions" >&2   ; exit 1    ;}
 [ -z "$RIDHOME"      ]  &&  { RIDHOME="/rid"                                    ;}
 [ -z "$RIDMETA"      ]  &&  { RIDMETA="$RIDHOME/meta"                           ;}
 [ -z "$RIDSETS"      ]  &&  { RIDMETA="$RIDHOME/sets"                           ;}
 [ -z "$RIDBIN"       ]  &&  { RIDBIN="$RIDBIN/bin"                              ;}
 [ -z "$RIDSOURCES"   ]  &&  { RIDSOURCES="/sources"                             ;}
 [ -e "$RIDPKGSJSON"  ]  &&  { echo "Backing up pkgs.json"           ; BACKUP=1  ;}
-[ -n "$BACKUP"       ]  &&  { mv -ivf "$RIDPKGSJSON" /tmp/ridpkgsjson.bak       ;}
+[ -n "$BACKUP"       ]  &&  { sudo mv -ivf "$RIDPKGSJSON" /tmp/ridpkgsjson.bak  ;}
 
-mkdir -pv "$RIDSOURCES" "$RIDHOME" "$RIDMETA"
+sudo mkdir -pv "$RIDSOURCES" "$RIDHOME" "$RIDMETA"
 
 echo "Pulling latest changes..."
 cd "$RIDHOME"
-git config init.defaultBranch master
-git init
-git remote set-url origin https://github.com/Toxikuu/rid.git
-git pull
+sudo git config init.defaultBranch master
+sudo git init
+sudo git remote set-url origin https://github.com/Toxikuu/rid.git
+sudo git pull
 
 cd "$RIDMETA"
-git config init.defaultBranch master
-git init
-git remote set-url origin https://github.com/Toxikuu/rid-meta.git
-git pull
+sudo git config init.defaultBranch master
+sudo git init
+sudo git remote set-url origin https://github.com/Toxikuu/rid-meta.git
+sudo git pull
     
 echo "Building rid..."
 cd   "$RIDHOME"
-cargo build --release
-cargo strip             || true # in case the user doesn't have cargo strip
-ln -sfv                 \
-     "$RIDHOME"/rid.sh  \
+sudo cargo build --release
+sudo cargo strip             || true # in case the user doesn't have cargo strip
+sudo ln -sfv                 \
+     "$RIDHOME"/rid.sh       \
      /usr/bin/rid
 
 echo 'Setting $RIDENV...'
-echo "export RIDENV=$RIDHOME/env" >> /etc/profile
+echo "export RIDENV=$RIDHOME/env" | sudo tee -a /etc/profile > /dev/null
 
-[ -n "$BACKUP"      ]   &&  { mv -vf /tmp/ridpkgsjson.bak "$RIDPKGSJSON"        ;}
+[ -n "$BACKUP"      ]   &&  { sudo mv -vf /tmp/ridpkgsjson.bak "$RIDPKGSJSON"   ;}
 
 popd    >/dev/null
 echo "Done!"
