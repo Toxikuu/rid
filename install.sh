184af9c3fe33583a6002899938c89141208b4288
Tox <toxikuu@users.noreply.github.com>
2024-11-18 23:42:44 -0600

    fixed overhaul

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # this script should be run with sudo
 
+[ "$EUID" -ne 0   ]  &&  { echo -e "\x1b[31;1m  Run this script as root\x1b[0m" ; exit 1 ;}
+
 [ -z "$SUDO_USER" ]  &&  SUDO_USER="$TARGET_USER"
 [ -z "$SUDO_USER" ]  &&  { echo -e "\x1b[31;1m  Run this script with sudo :)\x1b[0m" >&2 ; exit 1 ;}
 
 TU="$SUDO_USER"
 [ -z "$TU" ] && { echo -e "\x1b[31;1m  Could not determine target user!\x1b[0m" >&2; exit 1; }
-PATH="/usr/bin:/usr/sbin:/opt/cargo/bin"
 
 set -e
 pushd . >/dev/null
@@ -17,21 +18,25 @@ pushd . >/dev/null
 [ -z "$RIDSOURCES"   ]  &&  { RIDSOURCES="/sources"     ;}
 
 mkdir -pv "$RIDHOME" "$RIDMETA"
-chown -vR "$TU:$TU"  "$RIDHOME" "$RIDMETA"
+chown -R  "$TU:$TU"  "$RIDHOME" "$RIDMETA"
 
 su -p "$TU" -c '
+PATH="/usr/bin:/usr/sbin:/opt/cargo/bin"
+
 echo -e "\x1b[36;1m  Pulling latest changes...\x1b[0m"
 if [ ! -e "$RIDHOME"/.git ]; then
     git clone "https://github.com/Toxikuu/rid.git" "$RIDHOME"
 else
     cd "$RIDHOME" && git pull
 fi
+echo "Pulled for RIDHOME ($RIDHOME)"
 
 if [ ! -e "$RIDMETA"/.git ]; then
     git clone "https://github.com/Toxikuu/rid-meta.git" "$RIDMETA"
 else
     cd "$RIDMETA" && git pull
 fi
+echo "Pulled for RIDMETA ($RIDMETA)"
 
 echo -e "\x1b[36;1m  Building rid...\x1b[0m"
 cd   "$RIDHOME"
