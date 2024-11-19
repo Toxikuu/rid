2e4dc7b93752233eb6a6f2e8d5a2ab91b2d70d4b
Tox <toxikuu@users.noreply.github.com>
2024-11-18 22:37:28 -0600

    more improvements to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # this script should be run with sudo
 
+[ -z "$SUDO_USER"    ]  &&  { echo -e "\x1b[31;1m  Run this script with sudo :)\x1b[0m" >&2 ; exit 1 ;}
+
 set -e
 pushd . >/dev/null
 [ -e "$RIDENV"       ]  &&  . "$RIDENV"
@@ -12,7 +14,7 @@ PATH="/usr/bin:/usr/sbin:/opt/cargo/bin"
 [ -z "$RIDBIN"       ]  &&  { RIDBIN="$RIDBIN/bin"      ;}
 [ -z "$RIDSOURCES"   ]  &&  { RIDSOURCES="/sources"     ;}
 
-echo "Pulling latest changes..."
+echo -e "\x1b[36;1m  Pulling latest changes...\x1b[0m"
 if [ ! -e "$RIDHOME"/.git ]; then
     git clone "https://github.com/Toxikuu/rid.git" "$RIDHOME"
 else
@@ -25,7 +27,7 @@ else
     cd "$RIDMETA" && git pull
 fi
     
-echo "Building rid..."
+echo -e "\x1b[36;1m  Building rid...\x1b[0m"
 cd   "$RIDHOME"
 cargo build --release
 cargo strip             || true # in case the user doesn't have cargo strip
@@ -33,11 +35,11 @@ ln -sfv                 \
      "$RIDHOME"/rid.sh  \
      /usr/bin/rid
 
-echo 'Setting $RIDENV...'
+echo -e "\x1b[36;1m  Setting \$RIDENV...\x1b[0m"
 echo "export RIDENV=$RIDHOME/env" | tee -a /etc/profile > /dev/null
 
 mkdir -pv "$RIDSOURCES"
 
 popd    >/dev/null
 sudo chown -R $SUDO_USER:$SUDO_USER $RIDHOME # convenient as fuck for me; adjust this to your liking
-echo "Done!"
+echo "\x1b[36;1m  Done!\x1b[0m"
