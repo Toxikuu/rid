446b093723a532d5a91ee37958cfc39e99493b3a
Tox <toxikuu@users.noreply.github.com>
2024-11-18 23:37:40 -0600

    overhauled install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 # this script should be run with sudo
 
-[ -z "$SUDO_USER"    ]  &&  { echo -e "\x1b[31;1m  Run this script with sudo :)\x1b[0m" >&2 ; exit 1 ;}
+[ -z "$SUDO_USER" ]  &&  SUDO_USER="$TARGET_USER"
+[ -z "$SUDO_USER" ]  &&  { echo -e "\x1b[31;1m  Run this script with sudo :)\x1b[0m" >&2 ; exit 1 ;}
+
+TU="$SUDO_USER"
+[ -z "$TU" ] && { echo -e "\x1b[31;1m  Could not determine target user!\x1b[0m" >&2; exit 1; }
+PATH="/usr/bin:/usr/sbin:/opt/cargo/bin"
 
 set -e
 pushd . >/dev/null
-PATH="/usr/bin:/usr/sbin:/opt/cargo/bin"
+pushd . >/dev/null
 
 [ -z "$RIDHOME"      ]  &&  { RIDHOME="/rid"            ;}
-[ -z "$RIDMETA"      ]  &&  { RIDMETA="$RIDHOME/meta"   ;}
+[ -z "$RIDMETA"      ]  &&  { RIDMETA="/var/rid/meta"   ;}
 [ -z "$RIDSOURCES"   ]  &&  { RIDSOURCES="/sources"     ;}
 
+mkdir -pv "$RIDHOME" "$RIDMETA"
+chown -vR "$TU:$TU"  "$RIDHOME" "$RIDMETA"
+
+su -p "$TU" -c '
 echo -e "\x1b[36;1m  Pulling latest changes...\x1b[0m"
 if [ ! -e "$RIDHOME"/.git ]; then
     git clone "https://github.com/Toxikuu/rid.git" "$RIDHOME"
@@ -23,20 +32,16 @@ if [ ! -e "$RIDMETA"/.git ]; then
 else
     cd "$RIDMETA" && git pull
 fi
-    
+
 echo -e "\x1b[36;1m  Building rid...\x1b[0m"
 cd   "$RIDHOME"
 cargo build --release
-cargo strip             || true # in case the user doesn't have cargo strip
-ln -sfv                 \
-     "$RIDHOME"/rid.sh  \
-     /usr/bin/rid
+cargo strip             || true # in case the user doesnt have cargo strip
+'
 
-echo -e "\x1b[36;1m  Writing to /etc/profile...\x1b[0m"
-echo "export RIDHOME=$RIDHOME" | tee -a /etc/profile > /dev/null
-
-mkdir -pv "$RIDSOURCES"
+ln -sfv "$RIDHOME"/rid.sh /usr/bin/rid
+grep -qxF "export RIDHOME=$RIDHOME" /etc/profile ||
+     echo "export RIDHOME=$RIDHOME" >> /etc/profile
 
 popd    >/dev/null
-sudo chown -R $SUDO_USER:$SUDO_USER $RIDHOME # necessary if you want to git pull (this isn't very secure)
 echo -e "\x1b[36;1m  Done!\x1b[0m"
