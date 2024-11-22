9100557140652da275df1fc8185ee3e5281e1e50
Toxikuu <toxikuu@users.noreply.github.com>
2024-11-21 18:59:37 -0600

    renamed and added dependants

--- src/resolve.rs
+++ src/resolve.rs
@@ -1,9 +1,10 @@
-// src/resolvedeps.rs
+// src/resolve.rs
 //
-// responsible for dependency resolution
+// responsible for dependency/dependant resolution
 
 use crate::package::Package;
 use crate::vpr;
+use crate::utils::dedup;
 use std::collections::HashSet;
 
 fn deep_deps(pkg: &Package, pkglist: &Vec<Package>, resolved: &mut HashSet<String>, order: &mut Vec<String>) {
@@ -26,4 +27,25 @@ pub fn resolve_deps(pkg: &Package, pkglist: Vec<Package>) -> Vec<Package> {
     vpr!("Resolved dependencies: {:?}", order);
     let deps = order.iter().map(|d| Package::new(d, pkglist.clone())).collect::<Vec<Package>>();
     deps
+}
+
+pub fn find_dependants(pkg: Package, pkglist: Vec<Package>) -> Vec<Package> {
+    let mut dependants = pkglist
+        .iter()
+        .filter(|p| p.deps.contains(&pkg.name))
+        .inspect(|p| vpr!("Found dependant package: '{}'", p))
+        .cloned()
+        .collect::<Vec<Package>>();
+
+    dependants.retain(|p| p.name != *pkg.name);
+    dedup(dependants)
+}
+
+pub fn deep_dependants(deps: Vec<Package>, pkglist: &[Package]) -> Vec<Package> {
+    let mut dependants = Vec::new();
+    for dep in deps {
+        let dep_dependants = find_dependants(dep, pkglist.to_vec());
+        dependants.extend(dep_dependants);
+    }
+    dedup(dependants)
 }
