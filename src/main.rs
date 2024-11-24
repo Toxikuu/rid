13d6d981a074d03e3430bd287536035a6b1948a2
Toxikuu <toxikuu@users.noreply.github.com>
2024-11-24 16:13:13 -0600

    implemented caching changes && formatting

--- src/main.rs
+++ src/main.rs
@@ -26,22 +26,25 @@ fn main() {
     init::init();
     flags::set_flags(args.verbose, args.quiet, args.force);
 
-
     let pkgs = args.packages;
     let mut pkglist = load_pkglist();
     let pkgs = handle_sets(pkgs, &pkglist);
 
-    // could probably be a pm method(?)
-    if !args.cache {
+    let mut cache_list: Vec<String> = Vec::new();
+    if args.cache {
+        cache_list = pkgs.clone();
+        if cache_list.is_empty() { cache_list = pkglist.iter().map(|p| p.name.clone()).collect() }
+        msg!("Caching {} packages", cache_list.len());
+    } else {
         vpr!("Autocaching...");
-        match tracking::cache_changes(&mut pkglist, false) {
-            Ok(num) => vpr!("Autocached {} packages", num),
-            Err(e) => die!("Error autocaching: {}", e),
-        }
+    }
+
+    match tracking::cache_changes(&mut pkglist, cache_list) {
+        Ok(n) => vpr!("Cached {} packages", n),
+        Err(e) => die!("Error caching: {}", e),
     }
 
     let pkgs = pkgs.iter().map(|pkg| Package::new(pkg, pkglist.clone())).collect::<Vec<Package>>();
-
     let mut pm = PM::new(pkgs, pkglist);
 
     macro_rules! invoke{
@@ -56,7 +59,6 @@ fn main() {
 
     invoke!(args, pm, [
         list,
-        cache,
         dependencies,
         dependants,
         get,
