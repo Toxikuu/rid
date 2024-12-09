718b906c0a3c8260c7d9ea804a9cd777a4e14544
Toxikuu <toxikuu@users.noreply.github.com>
2024-12-09 00:00:58 -0600

    specified removal of $RID{BUILDING,EXTRACTION}

--- rid.sh
+++ rid.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 REPO="${REPO:-main}" . "$RIDHOME"/env || die "Failed to source dev rid's env"
-# echo "[rid.sh] REPO: $REPO" # debugging
+
+# the below is necessary to remove leftover source directories from packages that do not have links
+# TODO: move this functionality to rid
+[ -z "$RIDBUILDING" ] && die '$RIDBUILING not set'
+[ -z "$RIDHOME" ]     && die '$RIDHOME not set'
+rm -rf "$RIDBUILDING"/*
 
 "$RIDHOME"/target/release/rid "$@"
