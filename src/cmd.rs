693341ab745350b6105c4f369a4913bf2384f156
Toxikuu <toxikuu@users.noreply.github.com>
2024-12-10 19:02:19 -0600

    added color config support

--- src/cmd.rs
+++ src/cmd.rs
@@ -67,7 +67,7 @@ pub fn exec(command: &str) -> io::Result<()> {
         for line in reader.lines() {
             match line {
                 Ok(line) => {
-                    pr!("\x1b[31;1;3m{}", line);
+                    pr!("\x1b[{}{}", CONFIG.colors.stderr, line);
                     let log_line = format!("[ERR] {}\n", line);
                     let mut log_file = log_file_stderr.lock().unwrap();
                     let _ = write!(log_file, "{}", log_line);
--- src/macros.rs
+++ src/macros.rs
@@ -7,7 +7,8 @@ macro_rules! pr {
     ($($arg:tt)*) => {{
         use $crate::flags::QUIET;
         if !*QUIET.lock().unwrap() {
-            println!("\x1b[30;3m{}\x1b[0m", format!($($arg)*))
+            use $crate::config::CONFIG;
+            println!("\x1b[{}{}\x1b[0m", CONFIG.colors.default, format!($($arg)*))
         }
     }};
 }
@@ -17,40 +18,45 @@ macro_rules! vpr {
     ($($arg:tt)*) => {{
         use $crate::flags::VERBOSE;
         if *VERBOSE.lock().unwrap() {
+            use $crate::config::CONFIG;
             let f = std::path::Path::new(file!())
                 .file_name()
                 .and_then(|n| n.to_str())
                 .unwrap_or("Unknown");
-            println!("\x1b[34;1m[{}] {}\x1b[0m", f, format!($($arg)*))
+            println!("\x1b[{}[{}] {}\x1b[0m", CONFIG.colors.verbose, f, format!($($arg)*))
         }
     }};
 }
 
 #[macro_export]
 macro_rules! erm {
-    ($($arg:tt)*) => {
-        eprintln!("\x1b[31;1m  {}\x1b[0m", format!($($arg)*))
-    };
+    ($($arg:tt)*) => {{
+        use $crate::config::CONFIG;
+        eprintln!("\x1b[{}{}\x1b[0m", CONFIG.colors.danger, format!($($arg)*))
+    }};
 }
 
 #[macro_export]
 macro_rules! msg {
-    ($($arg:tt)*) => {
-        println!("\x1b[36;1m  {}\x1b[0m", format!($($arg)*))
-    };
+    ($($arg:tt)*) => {{
+        use $crate::config::CONFIG;
+        println!("\x1b[{}{}\x1b[0m", CONFIG.colors.message, format!($($arg)*))
+    }};
 }
 
 #[macro_export]
 macro_rules! die {
-    ($($arg:tt)*) => {
-        panic!("\x1b[31;1m  {}\x1b[0m", format!($($arg)*))
-    };
+    ($($arg:tt)*) => {{
+        use $crate::config::CONFIG;
+        panic!("\x1b{}{}\x1b[0m", CONFIG.colors.danger, format!($($arg)*))
+    }};
 }
 
 #[macro_export]
 macro_rules! yn {
     ($question:expr, $default:expr) => {{
         use std::io::{self, Write};
+        use $crate::config::CONFIG;
         let mut answer = $default;
         loop {
             let default_text = match $default {
@@ -58,7 +64,7 @@ macro_rules! yn {
                 false => "y/N",
             };
 
-            print!("\x1b[35;1m  {} ({}): \x1b[0m", $question, default_text);
+            print!("\x1b[{}{} ({}): \x1b[0m", CONFIG.colors.prompt, $question, default_text);
             io::stdout().flush().unwrap();
             let mut input = String::new();
             io::stdin()
