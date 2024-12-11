f1c0161dba39d9f9d02eeb685ddf9097c6551cf2
Toxikuu <toxikuu@users.noreply.github.com>
2024-12-10 18:14:51 -0600

    added config support

---
+++ src/config.rs
@@ -0,0 +1,60 @@
+// config.rs
+// responsible for parsing the config.toml
+
+use lazy_static::lazy_static;
+use std::sync::Arc;
+use serde::Deserialize;
+use std::path::Path;
+use std::fs;
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub behavior: BehaviorConfig,
+    pub colors: ColorsConfig,
+    pub linkval: LinkvalConfig,
+    pub upstream: LinkvalConfig,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct LinkvalConfig {
+    pub retry_count: u8,
+    pub stack_size: usize,
+    pub thread_count: usize,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct UpstreamConfig {
+    pub retry_count: u8,
+    pub stack_size: usize,
+    pub thread_count: usize,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ColorsConfig {
+    pub danger: String,
+    pub default: String,
+    pub message: String,
+    pub prompt: String,
+    pub verbose: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct BehaviorConfig {
+    pub remove_tarballs: bool,
+    pub search_threshold: u8,
+}
+
+impl Config {
+    pub fn load<P: AsRef<Path>>(path: P) -> Result<Self, Box<dyn std::error::Error>> {
+        let content = fs::read_to_string(path)?;
+        let config: Config = toml::from_str(&content)?;
+
+        Ok(config)
+    }
+}
+
+lazy_static! {
+    pub static ref CONFIG: Arc<Config> = Arc::new(
+        Config::load("/rid/config.toml").expect("Failed to load config.toml")
+    );
+}
