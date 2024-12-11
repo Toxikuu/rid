0772a93ff2ba27b2391e5393cc6db1bc0961c7c3
Toxikuu <toxikuu@users.noreply.github.com>
2024-12-10 19:50:33 -0600

    spacing tweak

--- src/flags.rs
+++ src/flags.rs
@@ -6,13 +6,13 @@ use lazy_static::lazy_static;
 use std::sync::Mutex;
 
 lazy_static! {
-    pub static ref FORCE: Mutex<bool> = Mutex::new(false);
-    pub static ref QUIET: Mutex<bool> = Mutex::new(false);
+    pub static ref FORCE:   Mutex<bool> = Mutex::new(false);
+    pub static ref QUIET:   Mutex<bool> = Mutex::new(false);
     pub static ref VERBOSE: Mutex<bool> = Mutex::new(false);
 }
 
 pub fn set_flags(force: bool, quiet: bool, verbose: bool) {
-    *FORCE.lock().unwrap() = force;
-    *QUIET.lock().unwrap() = quiet;
+    *FORCE.lock().unwrap()   = force;
+    *QUIET.lock().unwrap()   = quiet;
     *VERBOSE.lock().unwrap() = verbose;
 }
