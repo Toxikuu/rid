43ed5901a629af76f4fdcdd5c7812a9ad850f01b
Toxikuu <toxikuu@users.noreply.github.com>
2024-12-10 22:40:09 -0600

    added examples.rs to hold some utility functions for examples/*

---
+++ src/examples.rs
@@ -0,0 +1,37 @@
+// examples.rs
+//
+// defines helper functions for examples
+
+use std::io::{self, Write};
+use std::process::Command;
+use std::thread::sleep;
+use std::time::Duration;
+use whoami::username;
+
+fn check_perms() {
+    if username() != "root" {
+        panic!("Examples must be run as root");
+    }
+}
+
+pub fn e2c() {
+    println!("Press enter to continue...");
+    io::stdout().flush().unwrap();
+    let _ = io::stdin().read_line(&mut String::new());
+}
+
+// used for examples
+pub fn rid_cmd(args: &[&str]) {
+    check_perms();
+
+    println!(" $ rid {}\n", args.join(" "));
+    sleep(Duration::from_millis(1337));
+
+    let o = Command::new("rid")
+        .args(args)
+        .output()
+        .expect("Failed to execute command");
+
+    println!("{}", String::from_utf8_lossy(&o.stdout));
+    e2c();
+}
